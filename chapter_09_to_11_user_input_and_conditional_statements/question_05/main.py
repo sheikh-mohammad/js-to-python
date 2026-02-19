9bbd1c095e227149542e458f97a474ac823bacc7
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-19 16:44:22 +0500

    feat: question 5 of chapter 9 to 11

---
+++ chapter_09_to_11_user_input_and_conditional_statements/question_05/main.py
@@ -0,0 +1,47 @@
+# Part A
+
+a = 4
+if a + 1 == 5:
+    print("given condition for variable a is true")
+
+# Part B
+
+b = 82
+if b + 1 == 83:
+    print("given condition for variable b is true")
+
+# Part C
+
+c = 12
+if c + 1 == 13:
+    print("condition 1 is true")
+
+if c == 13:
+    print("condition 2 is true")
+
+if 1 + c < 14:
+    print("condition 3 is true")
+
+if c == 14:
+    print("condition 4 is true")
+
+# Part D
+
+materialCost = 20000
+laborCost = 2000
+totalCost = materialCost + laborCost
+if totalCost == laborCost + materialCost:
+    print("The cost equals")
+
+# Part E
+
+if True:
+    print("True")
+
+if False:
+    print("False")
+
+# Part F
+
+if "car" < "cat":
+    print("car is smaller than cat")
